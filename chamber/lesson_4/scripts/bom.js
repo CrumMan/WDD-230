3d112fd6f3a6147de38403b005af956f05179a9d
CrumMan <122419541+CrumMan@users.noreply.github.com>
2023-05-18 10:52:56 -0600

    index bom change

--- chamber/lesson_4/scripts/bom.js
+++ chamber/lesson_4/scripts/bom.js
@@ -1,14 +1,22 @@
 const button = document.querySelector("button")
 const userinput = document.querySelector("#favchap")
 const mylist = document.querySelector("#list")
+  // create an li element
+
+let newlistitem = document.createElement('li')
+
+  // create a delete button
+
+ let deletebutton = document.createElement('button')
 button.addEventListener('click', () =>{
-										if(userinput.value== ""){return}														newlistitem=document.createElement('li')
+	if(userinput.value== ""){return}														
+newlistitem=document.createElement('li')
 deletebutton= document.createElement('button')
 newlistitem.textContent=userinput.value
 deletebutton.textContent= '❌'
 newlistitem.append(deletebutton)
 mylist.append(newlistitem)
-deletebutton.addEventListner('click',() =>{mylist.remove.newlistitem})
+deletebutton.addEventListener('click',() =>{newlistitem.remove()})
 userinput.focus()
 userinput.value = ''
 })
