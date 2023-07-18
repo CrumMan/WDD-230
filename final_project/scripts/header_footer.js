795fc368a5614019bbb319df0015dd707edde59d
CrumMan <122419541+CrumMan@users.noreply.github.com>
2023-07-17 18:41:08 -0600

    final assignment 7/17 sessions 1 and 2

---
+++ final_project/scripts/header_footer.js
@@ -0,0 +1,10 @@
+menubutton = document.querySelector("#menu_button")
+
+menubutton.addEventListener('click', () =>
+{
+    document.querySelector('#navbar').classList.toggle('active');
+    document.querySelector('#menu_open').classList.toggle('active');
+    document.querySelector('#menu_open').classList.toggle('active');
+}
+
+)
